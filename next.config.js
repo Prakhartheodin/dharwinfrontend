d2d2d2d9eba83b1fa9bb56cf352dd3083ebc4528
Nishant Jain <nishantjain@Nishants-MacBook-Pro.local>
2026-02-02 12:28:10 +0530

    up

---
+++ next.config.js
@@ -0,0 +1,21 @@
+
+/**@type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  // basePath: isProd ? "/tailwind/app/dharwin-business-solutions/preview" : undefined,
+  // assetPrefix : isProd ? "/tailwind/app/dharwin-business-solutions/preview" : undefined,
+  basePath: "",
+  assetPrefix: "",
+  images: {
+    loader: "imgix",
+    path: "/",
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
